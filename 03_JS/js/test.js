9c27950db3d0ece1b3ad2ec28eecc5572f7f3349
Kweon <109863666+su5398@users.noreply.github.com>
2024-08-29 17:37:27 +0900

    Add files via upload

---
+++ 03_JS/js/test.js
@@ -0,0 +1,9 @@
+function test02(){
+    window.alert("외부 작성 방식");
+}
+// onclick
+window.onload=function(){
+    alert("윈도우 로딩 후! 화면 로딩 후!")
+
+    // document.getElementsByTagName("li")[2].onclick=function(){}
+}
